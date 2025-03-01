92dd7f16235cdc93cb652855e954a717c2cca640
realhoffmann <julianhoffmann21@gmail.com>
2025-03-01 16:46:17 +0100

    Initial Commit

---
+++ F1DriverCard/F1DriverCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  F1DriverCardApp.swift
+//  F1DriverCard
+//
+//  Created by Julian Hoffmann on 01.03.25.
+//
+
+import SwiftUI
+
+@main
+struct F1DriverCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
